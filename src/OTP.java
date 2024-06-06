0b57ad02f67772fd051c4f032a51e2b7baca9a0f
Fabse120 <fabseH60@gmail.com>
2024-06-06 10:35:45 +0200

    Created components for key creation.

--- src/OTP.java
+++ src/OTP.java
@@ -10,30 +10,43 @@ import java.security.NoSuchAlgorithmException;
 
 public class OTP {
 
-    private long now;
     private Cipher aes;
+//    private byte[] key = new byte[4];
     private byte[] encrypted;
-    private byte[] key = {};
+
 
     public String run() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        getSystemTime();
-        generateAES();
+        byte uid = (byte) 22;
+        byte OTP = (byte) 16;
+        byte validPer = (byte) 30;
+
+        generateAES(uid, OTP, validPer, getSystemTime());
+
         encrypt();
         return "";
     }
 
-    private void getSystemTime(){
-        this.now = System.currentTimeMillis() / 100;
+    private byte getSystemTime(){
+        long now = System.currentTimeMillis() / 100;
+        return (byte) now;
     }
 
-    private void generateAES() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    private byte[] generateAES(byte uid, byte OTPLength, byte validPer, byte timeNow) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        byte[] key = new byte[5];
+
+
+        key[0] = uid; // UID einfügen
+        key[1] = OTPLength; // OTP länge einfügen
+        key[2] = validPer; // Gültigkeitsdauer einfügen
+        key[3] = timeNow;
+
 
         this.aes = Cipher.getInstance("");
 
-        SecretKeySpec aesKey = new SecretKeySpec(this.key, "");
+        SecretKeySpec aesKey = new SecretKeySpec(key, "");
 
         this.aes.init(Cipher.ENCRYPT_MODE, aesKey);
-
+        return key;
     }
 
     private void encrypt() throws IllegalBlockSizeException, BadPaddingException {
